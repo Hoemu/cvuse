3a4377e2ed2d6763d6f9f90862f8846f62bb8174
HoeMuiiii <2501590635@qq.com>
2025-05-11 23:35:58 +0800

    配置 ffmpeg 运行环境

--- cvuse/CVUseFFmpeg/main.cpp
+++ cvuse/CVUseFFmpeg/main.cpp
@@ -1,11 +1,23 @@
-#include "mainwindow.h"
-
 #include <QApplication>
+#include <iostream>
+extern "C"
+{
+#include "./libavutil/log.h"
+}
+#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    // MainWindow w;
+    // w.show();
+    std::cout << "--------------- BEGIN --------------" << std::endl;
+
+    av_log_set_level(AV_LOG_DEBUG);
+
+    av_log(NULL, AV_LOG_INFO, "hello world!");
+
+    std::cout << "--------------- END --------------" << std::endl;
+
     return a.exec();
 }
